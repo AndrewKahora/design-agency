c16958aa36daacf1dceb5be8bb35e0b0429feca6
cahndrew <cahndrew@gmail.com>
2016-07-20 11:00:58 +0300

    members js

--- js/classie.js
+++ js/classie.js
@@ -83,3 +83,30 @@ if ( typeof define === 'function' && define.amd ) {
 }
 
 })( window );
+
+(function() {
+
+	function init() {
+		var speed = 250,
+			easing = mina.easeinout;
+
+		[].slice.call ( document.querySelectorAll( '#grid > a' ) ).forEach( function( el ) {
+			var s = Snap( el.querySelector( 'svg' ) ), path = s.select( 'path' ),
+				pathConfig = {
+					from : path.attr( 'd' ),
+					to : el.getAttribute( 'data-path-hover' )
+				};
+
+			el.addEventListener( 'mouseenter', function() {
+				path.animate( { 'path' : pathConfig.to }, speed, easing );
+			} );
+
+			el.addEventListener( 'mouseleave', function() {
+				path.animate( { 'path' : pathConfig.from }, speed, easing );
+			} );
+		} );
+	}
+
+	init();
+
+})();
